f3ad6f4bbcb9ca3ef98b8159ac7dbc291fdcad2b
Jonathan Dias Baraldi <jonathanbaraldi@gmail.com>
2016-02-15 15:08:51 -0200

    Correções

--- LerArquivo.js
+++ LerArquivo.js
@@ -1,6 +1,28 @@
 
 // Dependencias
-require('./conn.js');
+var AWS = require('aws-sdk');
+var express = require('express'); 
+var app = express();
+
+var bodyParser = require('body-parser');
+
+// Parsear o conteudo
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+// AWS
+var s3 = new AWS.S3();
+
+// Configuração da requisição, cabeçalhos, etc. CORS
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    // Métodos que queremos permitir
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 // GET
 app.get('/',function(req,res){
